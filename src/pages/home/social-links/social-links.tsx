efe8a37b5f63289950288335fc9c67ae10f52c6b
mgorunuch <mgorunuch.igor@gmail.com>
2020-08-16 21:30:21 +0300

    Add social links.

--- src/pages/home/social-links/social-links.tsx
+++ src/pages/home/social-links/social-links.tsx
@@ -6,9 +6,12 @@ class SocialLinks extends React.Component {
   render() {
     return (
       <div className="social-links">
+        <SocialLink iconName="linkedin" linkUrl="https://www.linkedin.com/in/mgorunuch" text="LinkedIn" />
+        <SocialLink iconName="twitter" linkUrl="https://twitter.com/mgorunuch" text="Twitter" />
+        <SocialLink iconName="telegram" linkUrl="https://t.me/mgorunuch" text="Telegram" />
         <SocialLink iconName="facebook" linkUrl="https://fb.com/mgorunuch" text="Facebook" />
         <SocialLink iconName="github" linkUrl="https://github.com/Mgorunuch" text="Github" />
-        <SocialLink iconName="stackoverflow" linkUrl="https://stackoverflow.com/users/5988531/mgorunuch" text="StackOverflow" />
+        {/*<SocialLink iconName="stackoverflow" linkUrl="https://stackoverflow.com/users/5988531/mgorunuch" text="StackOverflow" />*/}
       </div>
     );
   }
