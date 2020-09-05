38d5e38c3eccaf8adcc94331e956f65a15b07529
mgorunuch <mgorunuch.igor@gmail.com>
2020-09-05 20:08:55 +0300

    Add speed reading app link.

--- src/pages/home/my-apps/my-apps.tsx
+++ src/pages/home/my-apps/my-apps.tsx
@@ -9,6 +9,7 @@ class MyApps extends React.Component {
         <Link className="my-apps__link" to="wheel">- Life wheel</Link>
         <Link className="my-apps__link" to="priority">- Life priorities</Link>
         <Link className="my-apps__link" to="irregular-verbs">- Irregular verbs trainer</Link>
+        <a className="my-apps__link" href="https://speed-reading.mgorunuch.pro/" target="_blank">- Speed reading (BETA)</a>
       </div>
     );
   }
