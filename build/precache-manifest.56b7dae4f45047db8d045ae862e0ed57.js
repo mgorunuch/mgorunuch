1ee174ebc0d37d42c54da524f45329e64ef5f8a1
mgorunuch <mgorunuch.igor@gmail.com>
2021-01-03 13:40:06 +0200

    add build to heroku app

---
+++ build/precache-manifest.56b7dae4f45047db8d045ae862e0ed57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24e85496524e60d6d6b169a653f1c01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbaf4face5514e25801f",
+    "url": "/static/css/2.5a853316.chunk.css"
+  },
+  {
+    "revision": "ecd042f336c1254473c2",
+    "url": "/static/css/main.5075158c.chunk.css"
+  },
+  {
+    "revision": "cbaf4face5514e25801f",
+    "url": "/static/js/2.7fe78bad.chunk.js"
+  },
+  {
+    "revision": "ecd042f336c1254473c2",
+    "url": "/static/js/main.0b92a78c.chunk.js"
+  },
+  {
+    "revision": "2cc1e74b3a33e7cffdef",
+    "url": "/static/js/runtime-main.ca573fe2.js"
+  },
+  {
+    "revision": "c30c2a64b2bf44013123f52651a06741",
+    "url": "/static/media/creator.c30c2a64.jpg"
+  }
+]);
